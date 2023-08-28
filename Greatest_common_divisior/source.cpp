666c97f546afcf6e02f7a2825170347760061a4f
volker <kuntner.volker@fh-swf.de>
2023-08-28 06:09:32 +0200

    adding gcd exercise

---
+++ Greatest_common_divisior/source.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+
+int main() {
+
+  unsigned int a, b;
+
+  // user input
+  std::cout << "Please enter two integers\n";
+  std::cin >> a;
+  std::cin >> b;
+
+  // preparing for Euclidean algorithm
+  // determine wich input is the greater interger
+  if (a < b) {
+    unsigned int swap;
+    swap = a;
+    a = b;
+    b = swap;
+  }
+
+  // Euclidean algorithm
+  unsigned int gcd;
+  while (true) {
+    gcd = b;
+    b = a % b;
+    a = gcd;
+    if (b == 0) {
+      break;
+    }
+  }
+
+  std::cout << "\nThe greatest common divisor is " << gcd << std::endl;
+}
--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 This repository is an exercise for me to learn cpp. 
-For inspiration I use Marius Bancila, Die C++ Challange, dpunkt.verlag 2019, ISBN:978-3-86490-626-8.
+I use Marius Bancila, Die C++ Challange, dpunkt.verlag 2019, ISBN:978-3-86490-626-8.
